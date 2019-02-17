bcd206f8df9f3cbca6c3d0759633d0ede6e9e4a5
Yoromang Jatta <yjatta@cayorenterprises.com>
2019-02-17 13:45:46 +0000

    implemented terminal base web crawler ref @bucky

---
+++ spider.py
@@ -0,0 +1,80 @@
+from urllib.request import urlopen
+from linkFinder import LinkFinder
+from general import *
+
+
+class Spider:
+
+    # class variables shared among all instances
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
+    # bugs
+    # even upon error homepage still moving to crawled file
+    # duplicate links on a page adding to crawled file
+
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name =project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = project_name + '/queue.txt'
+        Spider.crawled_file = project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('first spider', Spider.base_url)
+
+    @staticmethod
+    def boot():
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + " now crawling " + page_url)
+            print("Queue " + str(len(Spider.queue)) + " | Crawled " + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.get_links(page_url))
+            Spider.queue.remove(page_url)  # remove crawled page from queue to avoid circular referencing
+            Spider.crawled.add(page_url)  # add the remove page above to crawled set
+            Spider.update_files()
+
+    @staticmethod
+    def get_links(page_url):
+        html_string = ''
+        try:
+            results = urlopen(page_url)
+            # if results.getheader('Content-Type') == 'text/html':
+            html_bytes = results.read()
+            html_string = html_bytes.decode("utf-8")
+            finder = LinkFinder(Spider.base_url, page_url)
+            finder.feed(html_string)
+
+        except:
+            print("Error cannot crawl page")
+            return set()
+        return finder.page_links()
+
+    @staticmethod
+    def add_links_to_queue(links):
+        for url in links:
+            if url in Spider.queue:
+                continue # don't add if it is already in the queue
+            if url in Spider.crawled:
+                continue  # don't add if is already crawled
+            if Spider.domain_name not in url:
+                continue  # don't add if links belongs to external website
+            Spider.queue.add(url)
+
+    @staticmethod
+    def update_files():
+        set_to_file(Spider.queue_file, Spider.queue)
+        set_to_file(Spider.crawled_file, Spider.crawled)
+
+
+
